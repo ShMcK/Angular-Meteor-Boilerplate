55ce7f31dea967dd530f3f6017d1a55a5233a763
ShMcK <me@shmck.com>
2015-07-01 16:01:00 +0900

    Item schema

--- model/items.js
+++ model/items.js
@@ -1,21 +1,62 @@
 ///<reference path="../typings/typings.d.ts" />
 Items = new Mongo.Collection('items');
-//Meteor.publish('items', function (options, searchString = '') {
-//
-//  Counts.publish(this, 'numberOfChallenges', Challenges.find({
-//    $or: [
-//      {'title': {'$regex': '.*' + searchString || '' + '.*', '$options': 'i'}},
-//      {'description1': {'$regex': '.*' + searchString || '' + '.*', '$options': 'i'}},
-//      {'description2': {'$regex': '.*' + searchString || '' + '.*', '$options': 'i'}}
-//    ]
-//  }), {noReady: true});
-//
-//  return Challenges.find({
-//    $or: [
-//      {'title': {'$regex': '.*' + searchString || '' + '.*', '$options': 'i'}},
-//      {'description1': {'$regex': '.*' + searchString || '' + '.*', '$options': 'i'}},
-//      {'description2': {'$regex': '.*' + searchString || '' + '.*', '$options': 'i'}}
-//    ]
-//  }, options);
-//});
+Schema.Item = new SimpleSchema({
+    _id: {
+        type: String,
+        optional: true
+    },
+    createdAt: {
+        type: Date,
+        optional: true
+    },
+    title: {
+        type: String,
+        optional: false,
+        editableBy: ['member', 'admin']
+    },
+    body: {
+        type: String,
+        optional: true,
+        editableBy: ['member', 'admin']
+    },
+    viewCount: {
+        type: Number,
+        optional: true
+    },
+    clickCount: {
+        type: Number,
+        optional: true
+    },
+    upVotes: {
+        type: Number,
+        optional: true
+    },
+    upVoters: {
+        type: [String],
+        optional: true
+    },
+    downVotes: {
+        type: Number,
+        optional: true
+    },
+    downVoters: {
+        type: [String],
+        optional: true
+    },
+    author: {
+        type: String,
+        optional: true
+    },
+    userId: {
+        type: String,
+        optional: true,
+        editable: ['admin']
+    }
+});
+//Schema.Item.internationalize();
+Items.attachSchema(Schema.Item);
+//Items.allow({
+//  update: function ():boolean {},
+//  remove: function ():boolean {}
+//}); 
 //# sourceMappingURL=items.js.map
