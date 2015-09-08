49cb8baadd77a1dbb07ec90d9ede3d65e8f4aebc
rajansen01 <r.a.jansen@live.nl>
2015-09-08 07:58:35 +0000

    removed tts file

--- client/zzz.ts-compiler.ts
+++
@@ -1,3 +0,0 @@
-// please add this file to .gitignore - it is used internally by typescript-compiler and must be the last file to compile
-// This needs some real logic so that tsc will use this dir as the starting path for all produced output
-if (true) {}
